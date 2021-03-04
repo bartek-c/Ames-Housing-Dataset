ffb01fe270518d90ecee21e725fa32097f2d4727
bartek-c <79876816+bartek-c@users.noreply.github.com>
2021-03-04 14:17:43 +0000

    Update random_forest.py

--- src/models/random_forest.py
+++ src/models/random_forest.py
@@ -1,9 +1,10 @@
 # imports
-import pandas as pd
-import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 
 # train random forest model
-rf_model = RandomForestRegressor(random_state=1,
-                                n_estimators=300)
-rf_model.fit(X_train, y_train)
+def train_rf(X_train, y_train):
+  rf_model = RandomForestRegressor(random_state=1,
+                                  n_estimators=300)
+  rf_model.fit(X_train, y_train)
+  
+  return rf_model
