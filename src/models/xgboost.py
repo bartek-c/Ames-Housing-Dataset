4188d7e4fd0cae8345c756510718df74f5035ed1
bartek-c <79876816+bartek-c@users.noreply.github.com>
2021-03-04 14:15:59 +0000

    Update xgboost.py

--- src/models/xgboost.py
+++ src/models/xgboost.py
@@ -1,11 +1,11 @@
 # imports
-import pandas as pd
-import numpy as np
 from xgboost import XGBRegressor
 
 # train XGBoost model
-xgboost_model = XGBRegressor(n_estimators=100, learning_rate=0.1)
-xgboost_model.fit(X_train, y_train, 
-             early_stopping_rounds=5, 
-             eval_set=[(X_val, y_val)], 
-             verbose=False)
+def train_xgboost(X_train, y_train, X_val, y_val):
+  xgboost_model = XGBRegressor(n_estimators=100, learning_rate=0.1)
+  xgboost_model.fit(X_train, y_train, 
+               early_stopping_rounds=5, 
+               eval_set=[(X_val, y_val)], 
+               verbose=False)
+  return xgboost_model
